d0c4f74e8f1f94f5a59592ff4312f871a8ea94b8
gabby <nabag123@newschool.edu>
2023-03-18 01:06:02 -0400

    Fixed third person cam and post processing

--- MovementGame/Assets/Scripts/ThirdPersonCam.cs
+++ MovementGame/Assets/Scripts/ThirdPersonCam.cs
@@ -29,6 +29,7 @@ public class ThirdPersonCam : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
+
         if (inputDir != Vector3.zero)
             playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
 
