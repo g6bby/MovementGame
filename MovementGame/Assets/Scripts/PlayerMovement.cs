2ed22483c4b6bc1520d8d9faaf372319edbf7b74
gabby <nabag123@newschool.edu>
2023-03-26 22:42:28 -0400

    Finished level 1 and level 2, finished game

--- MovementGame/Assets/Scripts/PlayerMovement.cs
+++ MovementGame/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+ 
+
     [Header("Movement")]
     public float moveSpeed;
     public float groundDrag;
@@ -58,15 +60,26 @@ public class PlayerMovement : MonoBehaviour
 
         //drag
         if (grounded)
+        {
             rb.drag = groundDrag;
+     
+        }
+
         else
+        {
             rb.drag = 0;
+    
+
+           
+        }
+
 
         //jump + double jump
         if (grounded && !Input.GetKey(KeyCode.Space))
         {
             doubleJump = false;
             isJumping = false;
+
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
@@ -86,6 +99,8 @@ public class PlayerMovement : MonoBehaviour
             {
                 rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y * 0.2f);
                 isJumping = true;
+           
+                
             }
 
             if (isJumping == false) 
